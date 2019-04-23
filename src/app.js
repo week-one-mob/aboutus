1948ab2fe5afb7f9347aed9b1e45004cad49958b
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-04-23 14:07:40 -0700

    create variables that hold input, and console logged them

--- src/app.js
+++ src/app.js
@@ -3,13 +3,19 @@
 const submitButton = document.getElementById ('submit-button');
 const drinkInput = document.getElementById ('drink-answer');
 const colorInput = document.getElementById ('color-answer');
-const animalInput= document.getElementById ('animal-answer');
+const animalInput = document.getElementById ('animal-answer');
 const heightInput = document.getElementById ('height-answer');
 const foodInput = document.getElementById ('food-answer');
 const quizResults = document.getElementById ('quiz-results');
 
 
-console.log("hello world");
 submitButton.addEventListener('click', () => {
     console.log("called");
+    const drinkInputAnswer = drinkInput.value;
+    const colorInputAnswer = colorInput.value;
+    const animalInputAnswer = animalInput.value;
+    const heightInputAnswer = heightInput.value;
+    const foodInputAnswer = foodInput.value;
+    console.log(drinkInputAnswer, colorInputAnswer, animalInputAnswer, heightInputAnswer, foodInputAnswer);
+
 });
