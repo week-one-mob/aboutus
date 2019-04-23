bf6492c7ca935bfe52f4bd29015aabe36c240c55
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-04-23 13:31:44 -0700

    created variables

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import scoreAnswers from './score-answers.js';
+
+const submitButton = document.getElementById ('submit-button');
+const drinkInput = document.getElementById ('drink-answer');
+const colorInput = document.getElementById ('color-answer');
+const animalInput= document.getElementById ('animal-answer');
+const heightInput = document.getElementById ('height-answer');
+const foodInput = document.getElementById ('food-answer');
+const quizResults = document.getElementById ('quiz-results');
+
+
